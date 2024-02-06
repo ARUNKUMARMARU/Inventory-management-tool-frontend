37e5103704e45ab24b409af96fd79bd254fbdc2a
ARUNKUMARMARU <arunsmart1020@gmail.com>
2024-02-06 11:54:05 +0530

    update base url

--- src/service/instance.js
+++ src/service/instance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURl = 'http://localhost:6001/api'; 
-const token = sessionStorage.getItem('token');
+const baseURl = 'https://inventory-management-tool-be.onrender.com/api'; 
 
 const authInstance =axios.create({
     baseURL : baseURl,
